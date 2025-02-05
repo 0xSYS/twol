4d7df4de72ed85cc84e9ee46799f1b8b684f5e75
0xsys <andreicu07@gmail.com>
2025-02-05 12:41:06 +0200

    Config stuff

--- src/config.hpp
+++ src/config.hpp
@@ -1,17 +1,20 @@
 #pragma once
 
-
+#include <string>
 
 
 
 // Global structure used for storing the app settings (io)
 typedef struct
 {
-  bool pc_status_mpack;   // Enable / disable pc status globally
-  bool cli_mode;          // Enable / disable cli arguments while tui is disabled
-  bool debug_log;         // Enable / disable debug log to file (logs are stored in /home/user/twol_logs)
-  bool restrict_autolink; // Enable linking the protected list files (linux only for now)
-  bool restrict_mode;     // Enable / disable restriction mode
+  bool pc_status_mpack;        // Enable / disable pc status globally
+  bool cli_mode;               // Enable / disable cli arguments while tui is disabled
+  bool debug_log;              // Enable / disable debug log to file (logs are stored in /home/user/twol_logs)
+  bool restrict_autolink;      // Enable linking the protected list files (linux only for now)
+  bool restrict_mode;          // Enable / disable restriction mode
+  bool restrict_timeout;       // Enable / disable restrict mode timeout
+  int rescrict_expr;           // Set ammount of time a rescricted session can last (in minutes)
+  std::string restr_list_path; // Set custom path for the encrypted pc list
 } cfg;
 
 
