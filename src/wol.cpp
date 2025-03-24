24221ce229522df70635ea5060f87658888ddad2
0xsys <andreicu07@gmail.com>
2025-03-24 11:06:16 +0200

    A very dumb mistake ;(

--- src/wol.cpp
+++ src/wol.cpp
@@ -11,12 +11,11 @@
  #include <arpa/inet.h>
  #include <sys/socket.h>
  #include <unistd.h>
+ #include <netdb.h>
 #endif
 
 
-#ifdef __WIN32
-  #include <netdb.h>
-
+#ifdef _WIN32
   #define WIN32_LEAN_AND_MEAN
   #include <windows.h>
   #include <winsock2.h>
